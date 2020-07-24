61774c4a9a80f39041b6b4d29d85e31c1842122f
brendo <admin@brendanmorrel.com>
2020-07-24 13:12:52 +0800

    .

--- config.def.h
+++ config.def.h
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "termite", NULL };
 
